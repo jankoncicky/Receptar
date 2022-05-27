e6275f02dcf785c9582a294dc3a09e8293628579
brandtalex <94475960+brandtalex@users.noreply.github.com>
2022-05-27 20:15:25 +0200

    pridani fotek stridani

--- stopky.js
+++ stopky.js
@@ -7,3 +7,24 @@ setInterval(() => {
 
     document.getElementById("cas2").innerText = newTime.toLocaleTimeString('cs-CZ', { timeZone: 'Europe/Prague' });
 }, 1000);
+
+
+let slideIndex = 0;
+zmena();
+
+function zmena() {
+  let i;
+  let slides = document.getElementsByClassName("slidy");
+  let tecky = document.getElementsByClassName("tecka");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < tecky.length; i++) {
+    tecky[i].className = tecky[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  tecky[slideIndex-1].className += " active";
+  setTimeout(zmena, 2000);
+}
