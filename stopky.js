ceca72809837975f2e37ef557b207a70cf0622b5
jankoncicky <94476976+jankoncicky@users.noreply.github.com>
2022-05-29 13:49:59 +0200

    adamovo upgrade

--- stopky.js
+++ stopky.js
@@ -3,7 +3,10 @@ var loadtime = Date.now();
 setInterval(() => {
     let newTime = new Date();
     let dif = new Date(newTime.getTime() - loadtime);
-    document.getElementById("cas3").innerText = "Aktuální čas: "+dif.toLocaleTimeString('cs-CZ', { timeZone: 'UTC' });
-
-    document.getElementById("cas2").innerText = "Čas strávený na naší stránce: "+newTime.toLocaleTimeString('cs-CZ', { timeZone: 'Europe/Prague' });
+    document.getElementById("cas3").innerText = "Aktuální čas: "+newTime.toLocaleTimeString('cs-CZ', { timeZone: 'Europe/Prague' });
 }, 1000);
+
+    document.getElementById("cas2").innerText = "Čas strávený na naší stránce: "+dif.toLocaleTimeString('cs-CZ', { timeZone: 'UTC' });
+
+
+
